516cf8ca017ee13bc2f50509982b05cb2c18aa73
raphaelbuenocaptacao-creator <raphaelbueno.captacao@gmail.com>
2026-08-25 18:48:38 -0300

    fix: stop stale PWA shell caching

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,8 @@
-const CACHE = 'crpay-v3';
-const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.svg', './icons/icon-512.svg'];
+const CACHE = 'crpay-v4';
+const OFFLINE = './index.html';
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)));
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.add(OFFLINE)));
   self.skipWaiting();
 });
 
@@ -15,13 +15,12 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
   event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        const clone = response.clone();
-        caches.open(CACHE).then((cache) => cache.put(event.request, clone));
-        return response;
-      })
-      .catch(() => caches.match(event.request).then((cached) => cached || caches.match('./index.html')))
+    fetch(event.request, { cache: 'no-store' }).catch(async () => {
+      const cached = await caches.match(event.request);
+      return cached || caches.match(OFFLINE);
+    })
   );
 });
